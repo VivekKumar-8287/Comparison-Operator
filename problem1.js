78fbfe3d07192134ccafb4c8326d16248ac6eea0
Vivek Kumar <104918205+VivekKumar-8287@users.noreply.github.com>
2022-12-29 16:13:34 +0000

    Initial commit

---
+++ problem1.js
@@ -0,0 +1,4 @@
+let min_pur=4000;
+let rahul_pur= 4500;
+
+console.log(min_pur <= rahul_pur)
---
+++ problem2.js
@@ -0,0 +1,12 @@
+
+
+let a = 10;
+let b = 20;
+let c = "10";
+
+console.log(a!=b);
+console.log(a==b);
+
+console.log(a!=b);
+console.log(a!==c);
+console.log(a===c);
